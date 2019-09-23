b0857bc74730a44954c40d0d1078920591b9dd84
Juliana <44816728+julianabahia@users.noreply.github.com>
2019-09-23 17:02:13 -0300

    Add files via upload

---
+++ estagio.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void tempo_de_servico(){
+    int salario;
+    int ano_de_contratacao;
+    char nome;
+
+    if(ano_de_contratacao <= 2019){
+        salario = salario*0.2;   
+    }
+}
+
+int main(){
+    char n;
+    int ano;
+    int sal;
+
+    printf("Digite o ano de entrada: ");
+    scanf("%d", &ano);
+    printf("Digite o salario: ");
+    scanf("%d", &sal);
+    printf("Digite o nome do estagiario: ");
+    scanf("%c", n);
+
+    tempo_de_servico(n, ano, sal);
+
+    printf("%c, %d, %d", &n, ano, sal);
+
+    return 0;
+
+}
